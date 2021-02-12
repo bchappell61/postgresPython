15f27bf8864a26ef0a403cf8cdd6b2468ef3803e
Bill-Chappell <bill@gistechsolutions.com>
2021-02-12 13:16:42 -0500

    First Commit

---
+++ ConnectQueryPostgres.py
@@ -0,0 +1,41 @@
+#pip install psycopg2
+import psycopg2
+
+conn = None
+
+print('Connecting to the PostgreSQL database...')
+conn = psycopg2.connect(
+    host="localhost",
+    database="<databaseName>",
+    user="postgres",
+    password="<thePassword>",
+    port="5432"
+    )
+
+# Query for:
+address = "1863 Mill Rd"
+
+# Query table for attributes The ? is a placeholder for a variable
+sql = """SELECT address, fixture_ty, pole_id
+         FROM lights
+         WHERE lights.address = (%s); """
+
+print('Connecting to the PostgreSQL database...')
+
+# create a cursor
+cur = conn.cursor()
+
+# address is the variable for the placeholder in sql query
+cur.execute(sql,(address,)) # Note comma after address needed Tuple
+
+# Fetchone brings back the matching record for that ID number
+result = cur.fetchone()
+
+print(result)
+#('1863 Mill Rd', 'Cobra', 21.0)
+
+# close the communication with the PostgreSQL
+cur.close()
+conn.close()
+print('Database connection closed.')
+
